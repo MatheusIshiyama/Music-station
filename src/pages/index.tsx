b2b3102949bb0a5efd61f9c1a925dfaedb03aa46
Matheus Ishiyama <matheus.ishiyama@outlook.com>
2021-05-04 16:43:22 -0300

    feat: set background image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+
+import styles from "@/src/styles/Home.module.scss";
+
+const defaultBackground =
+    "https://github.com/MatheusIshiyama/music-station/blob/main/public/defaultBackground.png?raw=true";
 
 export default function Home() {
-    return <div>Music station</div>;
+    const [backgroundUrl, setBackgroundUrl] = useState<string>();
+
+    return (
+        <div
+            style={{ backgroundImage: `url(${backgroundUrl ? backgroundUrl : defaultBackground})` }}
+            className={styles.container}
+        ></div>
+    );
 }
