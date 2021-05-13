db421dc24dfbc063df7a18c28a99178bdd9f6a30
Matheus Ishiyama <matheus.ishiyama@outlook.com>
2021-05-13 11:57:50 -0300

    refactor: identation

--- src/components/PlayerCard.tsx
+++ src/components/PlayerCard.tsx
@@ -5,20 +5,20 @@ import { usePlayer } from "@/src/context";
 import styles from "@/src/styles/Player.module.scss";
 
 const defaultThumbnail =
-  "https://github.com/MatheusIshiyama/music-station/blob/main/public/defaultBackground.png?raw=true";
+    "https://github.com/MatheusIshiyama/music-station/blob/main/public/defaultBackground.png?raw=true";
 
 export function PlayerCard() {
-  const { songs } = usePlayer();
+    const { songs } = usePlayer();
 
-  return (
-    <div className={styles.container}>
-      <h2>Music Station</h2>
-      <img
-        src={songs.length > 0 ? songs[0].thumbnail : defaultThumbnail}
-        alt=""
-        className={styles.thumbnail}
-      />
-      <h3>{songs.length > 0 ? songs[0].title : "Nenhuma música tocando"}</h3>
-    </div>
-  );
+    return (
+        <div className={styles.container}>
+            <h2>Music Station</h2>
+            <img
+                src={songs.length > 0 ? songs[0].thumbnail : defaultThumbnail}
+                alt=""
+                className={styles.thumbnail}
+            />
+            <h3>{songs.length > 0 ? songs[0].title : "Nenhuma música tocando"}</h3>
+        </div>
+    );
 }
